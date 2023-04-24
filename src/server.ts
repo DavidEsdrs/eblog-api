86ae54bad74cc8a6cdc093c42c4cab21b952b386
DavidEsdrs <carpina1983@gmail.com>
2023-04-24 20:42:50 -0300

    First route and express setup

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import express from "express";
+import { router } from "./router";
 
 const app = express();
 app.use(express.json());
-
-app.use("/", (req, res) => res.send("running"));
+app.use(router);
 
 const SERVER_PORT = process.env.SERVER_PORT || 3000;
 app.listen(SERVER_PORT, () => console.log(`running ${SERVER_PORT}`));

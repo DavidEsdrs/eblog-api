d70570f0fd702fb6df8e511636c28ee83d5603b6
DavidEsdrs <carpina1983@gmail.com>
2023-04-25 03:43:42 -0300

    Add user table creation migration

---
+++ src/migrations/1682389045477-create_users_table.ts
@@ -0,0 +1,47 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm"
+
+export class CreateUsersTable1682389045477 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+
+        await queryRunner.createTable(new Table({
+            name: "users",
+            columns: [
+                {
+                    name: "id",
+                    type: "integer",
+                    isPrimary: true,
+                    isGenerated: true,
+                    generationStrategy: "increment"
+                },
+                {
+                    name: "email",
+                    type: "varchar",
+                    isUnique: true,
+                    length: "100"
+                },
+                {
+                    name: "password",
+                    type: "varchar",
+                    length: "100"
+                },
+                {
+                    name: "created_at",
+                    type: "timestamp",
+                    default: "now()"
+                },
+                {
+                    name: "updated_at",
+                    type: "timestamp",
+                    default: "now()"
+                }
+            ]
+        }));
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        
+        await queryRunner.dropTable("users");
+    }
+
+}

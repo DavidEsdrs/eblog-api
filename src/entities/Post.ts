8250009e41adcf001abb232777808cd137611c0d
DavidEsdrs <carpina1983@gmail.com>
2023-04-25 11:53:37 -0300

    Add title and summary collumns in entity

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,10 +1,16 @@
-import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import { User } from "./User";
 
 @Entity("posts")
 export class Post {
     @PrimaryGeneratedColumn()
     readonly id: number;
+
+    @Column({ name: "title", type: "varchar", length: "100" })
+    title: string;
+
+    @Column({ name: "summary", type: "varchar", length: "255" })
+    summary: string;
 
     @Column({ name: "content", type: "longtext" })
     content: string;
@@ -15,6 +21,7 @@ export class Post {
     @UpdateDateColumn()
     updated_at: Date;
 
-    @ManyToOne(type => User)
+    @ManyToOne(type => User, user => user.id)
+    @JoinColumn({ referencedColumnName: "id", name: "creator" })
     creator: User;
 }

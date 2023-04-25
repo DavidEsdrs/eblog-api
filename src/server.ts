8b237ee4e9e363abfb94dfe55cb553b107d8eb0a
DavidEsdrs <carpina1983@gmail.com>
2023-04-25 03:33:28 -0300

    Config dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,17 @@
+import "reflect-metadata";
+import "express-async-errors";
 import express from "express";
 import { router } from "./router";
+import { config } from "dotenv";
+
+config({
+    path: ".env.dev"
+});
 
 const app = express();
 app.use(express.json());
 app.use(router);
 
 const SERVER_PORT = process.env.SERVER_PORT || 3000;
+
 app.listen(SERVER_PORT, () => console.log(`running ${SERVER_PORT}`));

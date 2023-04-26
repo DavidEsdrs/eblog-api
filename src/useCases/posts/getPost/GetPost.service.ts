83ec30ff8abca9db5ff2be0b21bf986041597b90
DavidEsdrs <carpina1983@gmail.com>
2023-04-26 11:33:02 -0300

    Use posts repository interface instead of typeorm repository && Select wanted columns instead of select all !

--- src/useCases/posts/getPost/GetPost.service.ts
+++ src/useCases/posts/getPost/GetPost.service.ts
@@ -1,14 +1,26 @@
 import { Repository } from "typeorm";
 import { Post } from "../../../entities/Post";
 import { IGetPostDTO } from "./GetPost.controller";
+import { IPostsRepository } from "../../../repositories/IPostsRepository";
 
 export class GetPostService {
     constructor(
-        private postsRepository: Repository<Post>
+        private postsRepository: IPostsRepository
     ) {}
 
     async execute({ id, user_id }: IGetPostDTO) {
-        const post = await this.postsRepository.findOne({ where: { id } });
+        const post = await this.postsRepository.findPostById(id, {
+            id: true,
+            title: true,
+            created_at: true,
+            creator: {
+                id: true,
+                email: true
+            },
+            summary: true,
+            featured_image: true,
+            updated_at: true
+        });
         if(!post) {
             throw new Error("The given post id doesn't exist in database!");
         }

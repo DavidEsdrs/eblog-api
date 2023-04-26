ee4d7ed1d709bb5534b5c90a3be7ec107b80b6ba
DavidEsdrs <carpina1983@gmail.com>
2023-04-26 11:33:35 -0300

    Implements posts repository with typeorm

--- src/repositories/IPostsRepository.ts
+++ src/repositories/IPostsRepository.ts
@@ -1,6 +1,8 @@
 import { Repository } from "typeorm";
 import { Post } from "../entities/Post";
+import { AsBoolean } from "./implementations/PostsRepository";
 
-export interface IPostsRepository extends Repository<Post> {
+export interface IPostsRepository {
     getFeaturedPosts({ limit, offset }: { limit: number, offset: number }): Promise<Post[]>;
+    findPostById(id: number, select?: AsBoolean<Post>): Promise<Post>;
 }
--- src/repositories/implementations/PostsRepository.ts
+++ src/repositories/implementations/PostsRepository.ts
@@ -1,6 +1,12 @@
 import { Post } from "../../entities/Post";
 import AppDataSource from "../../ormconfig";
 
+export type AsBoolean<T> = {
+    [P in keyof T]?: 
+        T[P] extends Date ? boolean : 
+            (T[P] extends object ? AsBoolean<T[P]> : boolean)
+};
+
 export const PostsRepository = AppDataSource.getRepository(Post).extend({
     async getFeaturedPosts({ limit = 10, offset = 0 }: { limit: number, offset: number }) {
         const posts = await this.createQueryBuilder("p").
@@ -8,5 +14,14 @@ export const PostsRepository = AppDataSource.getRepository(Post).extend({
             offset(offset).
             getMany();
         return posts;
+    },
+
+    async findPostById(id: number, select?: AsBoolean<Post>) {
+        const post = await this.findOne({ 
+            where: { id }, 
+            select, 
+            relations: select?.creator ? ["creator"] : [] 
+        });
+        return post;
     }
 });

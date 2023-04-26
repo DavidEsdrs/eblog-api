ca3affc5dd5594d53ef877a08052a6ac48e13fa1
DavidEsdrs <carpina1983@gmail.com>
2023-04-26 11:31:10 -0300

    Use Posts custom repository

--- src/useCases/posts/create/buildCreatePost.ts
+++ src/useCases/posts/create/buildCreatePost.ts
@@ -1,13 +1,13 @@
 import { Post } from "../../../entities/Post";
 import { User } from "../../../entities/User"
 import AppDataSource from "../../../ormconfig"
+import { PostsRepository } from "../../../repositories/implementations/PostsRepository";
 import { CreatePostController } from "./CreatePost.controller";
 import { CreatePostService } from "./CreatePost.service";
 
 export const buildCreatePost = () => {
     const usersRepository = AppDataSource.getRepository(User);
-    const postsRepository = AppDataSource.getRepository(Post);
-    const service = new CreatePostService(usersRepository, postsRepository);
+    const service = new CreatePostService(usersRepository, PostsRepository);
     const controller = new CreatePostController(service);
     return controller;
 }

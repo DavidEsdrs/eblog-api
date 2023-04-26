17093996b69e567d1316f546a2fe7fce6d12357b
DavidEsdrs <carpina1983@gmail.com>
2023-04-25 21:21:51 -0300

    Create error handling middleware

---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction } from "express";
+
+export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+    if(err instanceof Error) {
+        return res.json({ error: err.message });
+    } else {
+        return res.status(500).json({ error: "Internal server error!" });
+    }
+}

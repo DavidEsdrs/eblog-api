6c3053b312eba52109e0f8611fbf49d702958a2d
DavidEsdrs <carpina1983@gmail.com>
2023-04-26 06:28:17 -0300

    Change body validation function name && Create query validator util function

--- src/utils/createValidator.ts
+++ src/utils/createValidator.ts
@@ -1,13 +1,25 @@
 import { Request, Response, NextFunction } from "express";
 import { z } from "zod";
 
-export const createValidator = (schema: z.ZodType) => {
+export const createBodyValidator = (schema: z.ZodType) => {
     return async (req: Request, res: Response, next: NextFunction) => {
         try {
             await schema.parseAsync(req.body);
             return next();
         } catch(err) {
             throw new Error("Invalid body");
+        }
+    }
+}
+
+export const createQueryValidator = (schema: z.ZodType) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const parsedQuery = schema.parse(req.query);
+            req.query = parsedQuery;
+            return next();
+        } catch(err) {
+            throw new Error("Query params couldn't be parsed!");
         }
     }
 }

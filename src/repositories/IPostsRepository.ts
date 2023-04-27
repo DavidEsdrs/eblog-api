27a30f242af3ff8ba40e7be9632ff7c57b646d2a
DavidEsdrs <carpina1983@gmail.com>
2023-04-27 14:57:04 -0300

    Add updatePost function

--- src/repositories/IPostsRepository.ts
+++ src/repositories/IPostsRepository.ts
@@ -5,4 +5,5 @@ import { AsBoolean } from "./implementations/PostsRepository";
 export interface IPostsRepository {
     getFeaturedPosts({ limit, offset }: { limit: number, offset: number }): Promise<Post[]>;
     findPostById(id: number, select?: AsBoolean<Post>): Promise<Post>;
+    updatePost(id: number, { title, content, summary }: Pick<Partial<Post>, "title" | "summary" | "content">): Promise<void>;
 }

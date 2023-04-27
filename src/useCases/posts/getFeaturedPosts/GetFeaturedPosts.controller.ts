178c5f1116924475cd89daf2073fc9b7e5644070
DavidEsdrs <carpina1983@gmail.com>
2023-04-27 14:43:34 -0300

    Refactor: remove unnecessary interfaces

--- src/useCases/posts/getFeaturedPosts/GetFeaturedPosts.controller.ts
+++ src/useCases/posts/getFeaturedPosts/GetFeaturedPosts.controller.ts
@@ -1,10 +1,6 @@
 import { Request, Response } from "express";
 import { Post } from "../../../entities/Post";
-
-interface IGetFeaturedPostsDTO {
-    limit: number;
-    offset: number;
-}
+import { IGetFeaturedPostsDTO } from "./GetFeaturedPosts.dto";
 
 interface IGetFeaturedPostsService {
     execute(args: IGetFeaturedPostsDTO): Promise<Post[]>;
@@ -17,7 +13,6 @@ export class GetFeaturedPostsController {
 
     async handle(req: Request, res: Response) {
         const { limit, offset } = req.query;
-        console.log(req.query)
         const posts = await this.service.execute({ limit: Number(limit), offset: Number(offset) });
         return res.json(posts);
     }

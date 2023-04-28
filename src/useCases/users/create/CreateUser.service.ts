87c399434f8162ec8bc70ab1e6c3ebb08334ebb1
DavidEsdrs <carpina1983@gmail.com>
2023-04-28 13:21:05 -0300

    Throw 403 forbidden custom error when credentials already taken

--- src/useCases/users/create/CreateUser.service.ts
+++ src/useCases/users/create/CreateUser.service.ts
@@ -2,6 +2,7 @@ import { Repository } from "typeorm";
 import { User } from "../../../entities/User";
 import { ICreateUserDTO } from "./CreateUser.dto";
 import { hash } from "argon2";
+import { ForbiddenRequestError } from "../../../utils/httpErrors";
 
 export class CreateUserService {
     constructor(
@@ -11,7 +12,7 @@ export class CreateUserService {
     async execute({ email, password }: ICreateUserDTO) {
         const credentialsTaken = await this.usersRepository.findOne({ where: { email } });
         if(credentialsTaken) {
-            throw new Error("Credentials taken!");
+            throw new ForbiddenRequestError();
         }
         const pwdHash = await hash(password);
         const user = this.usersRepository.create({ email, password: pwdHash });

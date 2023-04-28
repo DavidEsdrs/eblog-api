760732ffd6e214c17143a594aa68be091ea708e0
DavidEsdrs <carpina1983@gmail.com>
2023-04-27 22:39:12 -0300

    Use cookie parser and cors configuration

--- src/server.ts
+++ src/server.ts
@@ -5,17 +5,26 @@ import { router } from "./router";
 import { config } from "dotenv";
 import { errorHandler } from "./middlewares/errorHandler";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 config({
     path: ".env.dev"
 });
 
 const app = express();
+app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    if(origin === process.env.FRONT_END_URL) {
+        res.header("Access-Control-Allow-Credentials", "true");
+    }
+    next();
+});
 app.use(cors({
     origin: process.env.FRONT_END_URL,
     credentials: true
 }));
 app.use(express.json());
+app.use(cookieParser());
 app.use(router);
 app.use(errorHandler);
 

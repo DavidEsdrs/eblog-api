d6fc3577d917b6fa6f0916c0c2932813e3e1f115
DavidEsdrs <carpina1983@gmail.com>
2023-04-28 13:22:26 -0300

    Add findById function and its implementation in users repository

--- src/repositories/IUsersRepository.ts
+++ src/repositories/IUsersRepository.ts
@@ -2,4 +2,5 @@ import { User } from "../entities/User";
 
 export interface IUsersRepository {
     create(args: Partial<User>): User;
+    findById(id: number): Promise<User>;
 }
--- src/repositories/implementations/UsersRepository.ts
+++ src/repositories/implementations/UsersRepository.ts
@@ -2,4 +2,8 @@ import { User } from "../../entities/User";
 import AppDataSource from "../../ormconfig";
 
 export const UsersRepository = AppDataSource.getRepository(User).extend({
+    async findById(id: number) {
+        const user = await this.findOne({ where: { id } });
+        return user;
+    }
 });

3ccbceffde97eb1c86166fddaea24de92e625e40
DavidEsdrs <carpina1983@gmail.com>
2023-04-28 13:20:24 -0300

    Throw 403 forbidden custom error when requester is not the creator of the post

--- src/useCases/posts/update/UpdatePost.service.ts
+++ src/useCases/posts/update/UpdatePost.service.ts
@@ -1,4 +1,5 @@
 import { IPostsRepository } from "../../../repositories/IPostsRepository";
+import { ForbiddenRequestError } from "../../../utils/httpErrors";
 import { IUpdatePostDTO } from "./UpdatePost.dto";
 
 export class UpdatePostService {
@@ -9,7 +10,7 @@ export class UpdatePostService {
     async execute({ id, title, content, summary, requester_id }: IUpdatePostDTO) {
         const post = await this.postsRepository.findPostById(id, { creator: { id: true } });
         if(post.creator.id !== requester_id) {
-            throw new Error("Invalid request!");
+            throw new ForbiddenRequestError();
         }
         await this.postsRepository.updatePost(id, { title, content, summary });
     }

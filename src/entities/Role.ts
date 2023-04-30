ad9fbad476caec3ccdb722c67c2663d40b0121f3
DavidEsdrs <carpina1983@gmail.com>
2023-04-30 19:29:59 -0300

    Remove nullable true in the user column to allow to create Role without a user

--- src/entities/Role.ts
+++ src/entities/Role.ts
@@ -1,13 +1,15 @@
 import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import { User } from "./User";
 
+export type Roles = "root" | "editor" | "author" | "reader";
+
 @Entity("roles")
 export class Role {
     @PrimaryGeneratedColumn()
     readonly id: number;
 
     @Column({ name: "type", type: "enum", enum: ["root", "editor", "author", "reader"] })
-    type: "root" | "editor" | "author" | "reader";
+    type: Roles;
 
     @CreateDateColumn()
     created_at: Date;
@@ -15,7 +17,7 @@ export class Role {
     @UpdateDateColumn()
     updated_at: Date;
 
-    @ManyToOne(type => User, user => user.roles, { nullable: false })
+    @ManyToOne(type => User, user => user.roles)
     @JoinColumn({
         name: "user",
         referencedColumnName: "id"

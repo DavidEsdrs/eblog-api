9ac0c0e5ec17f4544a506f27c0b582eef0f55530
DavidEsdrs <carpina1983@gmail.com>
2023-05-14 20:43:10 -0300

    feat: Check user role permissions before save post in the database

--- src/useCases/posts/create/CreatePost.service.ts
+++ src/useCases/posts/create/CreatePost.service.ts
@@ -2,6 +2,8 @@ import { Repository } from "typeorm";
 import { ICreatePostDTO } from "./CreatePost.dto";
 import { User } from "../../../entities/User";
 import { Post } from "../../../entities/Post";
+import { PERSMISIONS } from "../../../configs/configs";
+import { Roles } from "../../../entities/Role";
 
 export class CreatePostService {
     constructor(
@@ -14,8 +16,27 @@ export class CreatePostService {
         if(!user) {
             throw new Error("User doesn't exist!");
         }
+        if(!this.checkPermission(user)) {
+            throw new Error("User doesn't have permision!");
+        }
         const post = this.postsRepository.create({ content, creator: user, summary, title, featured_image });
         await this.postsRepository.save(post);
         return post;
+    }
+
+    private checkPermission(user: User): boolean {
+        const roles = user.roles.map(role => role.type);
+        for (let role of roles) {
+            const permissions = this.getPermissionForRole(role);
+            if(permissions.write) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private getPermissionForRole(role: Roles) {
+        const [roleKey, permissions] = Object.entries(PERSMISIONS).find(([roleKey, permissions]) => roleKey === role);
+        return permissions;
     }
 }

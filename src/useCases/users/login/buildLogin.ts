07b5e23bb513ca69cb710d4e00512486408635ed
DavidEsdrs <carpina1983@gmail.com>
2023-05-14 19:06:46 -0300

    refactor: Use custom user repository instead of standard typeorm repository

--- src/useCases/users/login/buildLogin.ts
+++ src/useCases/users/login/buildLogin.ts
@@ -1,14 +1,12 @@
-import { User } from "../../../entities/User"
-import AppDataSource from "../../../ormconfig"
 import { RefreshTokenProvider } from "../../../providers/RefreshTokenProvider";
 import { RefreshTokenRepository } from "../../../repositories/implementations/RefreshTokenRepository";
+import { UsersRepository } from "../../../repositories/implementations/UsersRepository";
 import { LoginController } from "./Login.controller";
 import { LoginService } from "./Login.service";
 
 export const buildLogin = () => {
-    const usersRepository = AppDataSource.getRepository(User);
-    const service = new LoginService(usersRepository);
-    const refreshTokenGenerator = new RefreshTokenProvider(RefreshTokenRepository, usersRepository);
+    const service = new LoginService(UsersRepository);
+    const refreshTokenGenerator = new RefreshTokenProvider(RefreshTokenRepository, UsersRepository);
     const controller = new LoginController(service, refreshTokenGenerator);
     return controller;
 }
